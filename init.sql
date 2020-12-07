2836f67a2de434137547a9952e029e4efbf9dd12
bokwoon <bokwoon.c@gmail.com>
2020-12-07 19:01:34 +0800

    checkpoint

--- init.sql
+++ init.sql
@@ -1,11 +1,29 @@
-DROP TABLE IF EXISTS wb_users_posts;
-DROP TABLE IF EXISTS wb_posts;
+-- wb
+DROP TRIGGER IF EXISTS blg_posts_after_insert;
+DROP TRIGGER IF EXISTS blg_posts_after_delete;
+DROP TRIGGER IF EXISTS blg_posts_after_update;
+DROP TABLE IF EXISTS blg_users_posts;
+DROP TABLE IF EXISTS blg_posts_fts_config;
+DROP TABLE IF EXISTS blg_posts_fts_data;
+DROP TABLE IF EXISTS blg_posts_fts_docsize;
+DROP TABLE IF EXISTS blg_posts_fts_idx;
+DROP TABLE IF EXISTS blg_posts_fts;
+DROP TABLE IF EXISTS blg_posts;
+DROP TABLE IF EXISTS blg_config;
+-- pm
 DROP TABLE IF EXISTS pm_routes;
+DROP TABLE IF EXISTS pm_templates;
 DROP TABLE IF EXISTS pm_users;
 
 -- pagemanager
 CREATE TABLE pm_users (
     user_id BIGINT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE pm_templates (
+    name TEXT NOT NULL PRIMARY KEY
+    ,body TEXT
+    ,data_format TEXT
 );
 
 CREATE TABLE pm_routes (
@@ -17,21 +35,73 @@ CREATE TABLE pm_routes (
 );
 
 -- blog
--- TODO: how to design blog tables such that it can accomodate arbitrary URL hierarchies?
-CREATE TABLE wb_posts (
+CREATE TABLE blg_config (
+    config_id INT NOT NULL PRIMARY KEY
+    ,json JSONB
+    ,pagination_format TEXT
+    ,posts_per_page INT
+    ,date_format TEXT
+    ,index_entry_format TEXT
+    ,url_format TEXT
+);
+
+CREATE TABLE blg_posts (
     post_id BIGINT NOT NULL PRIMARY KEY
     ,slug TEXT
     ,title TEXT
     ,summary TEXT
+    ,body TEXT
     ,published_on TIMESTAMPTZ
     ,unpublished_on TIMESTAMPTZ
     ,created_at TIMESTAMPTZ
+    ,updated_at TIMESTAMPTZ
 );
 
-CREATE TABLE wb_users_posts (
+-- https://kimsereylam.com/sqlite/2020/03/06/full-text-search-with-sqlite.html
+CREATE VIRTUAL TABLE blg_posts_fts USING FTS5 (
+    title
+    ,summary
+    ,body
+    ,content='blg_posts'
+    ,content_rowid='post_id'
+);
+
+CREATE TRIGGER blg_posts_after_insert AFTER INSERT ON blg_posts
+BEGIN
+    INSERT INTO blg_posts_fts
+        (rowid, title, summary, body)
+    VALUES
+        (NEW.post_id, NEW.title, NEW.summary, NEW.body)
+    ;
+END;
+
+CREATE TRIGGER blg_posts_after_delete AFTER DELETE ON blg_posts
+BEGIN
+    INSERT INTO blg_posts_fts
+        (blg_posts_fts, rowid, title, summary, body)
+    VALUES
+        ('delete', OLD.id, OLD.title, OLD.summary, OLD.body)
+    ;
+END;
+
+CREATE TRIGGER blg_posts_after_update AFTER UPDATE ON blg_posts
+BEGIN
+    INSERT INTO blg_posts_fts
+        (blg_posts_fts, rowid, title, summary, body)
+    VALUES
+        ('delete', OLD.id, OLD.title, OLD.summary, OLD.body)
+    ;
+    INSERT INTO blg_posts_fts
+        (rowid, title, summary, body)
+    VALUES
+        (NEW.id, NEW.title, NEW.summary, NEW.body)
+    ;
+END;
+
+CREATE TABLE blg_users_posts (
     user_id BIGINT
     ,post_id BIGINT
 
     ,FOREIGN KEY (user_id) REFERENCES pm_users (user_id)
-    ,FOREIGN KEY (post_id) REFERENCES wb_posts (post_id)
+    ,FOREIGN KEY (post_id) REFERENCES blg_posts (post_id)
 );
