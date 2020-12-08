4eec1a743ec7d0b12ec1a0c27a7a361d4fefe0c1
bokwoon <bokwoon.c@gmail.com>
2020-12-08 22:03:27 +0800

    checkpoint

--- bruh.sh
+++ bruh.sh
@@ -2,4 +2,4 @@
 pkill myserverproj && echo "Sent kill"
 rm -f ./myserverproj && echo "Removed old binary"
 echo "Going to build and run..."
-go build -o myserverproj simplewhite/main.go && ./myserverproj
+go build -o myserverproj plainsimple/main.go && ./myserverproj
